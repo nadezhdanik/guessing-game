cce3fe1cae28f1ffd21b78a56c07aeed16d09852
nadezhdanik <nadezhdanik@gmail.com>
2023-10-02 14:56:40 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.middle = Math.round((this.max + this.min) / 2);
+    return this.middle;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.middle;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.middle;
+  }
 }
 
 module.exports = GuessingGame;
